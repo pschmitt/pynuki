d38c87f75876580ab87fa1541de0010ad672e487
Philipp Schmitt <philipp@schmitt.co>
2020-01-25 15:35:53 +0100

    Implement opener actions

--- pynuki/opener.py
+++ pynuki/opener.py
@@ -6,4 +6,37 @@ from .utils import logger
 
 
 class NukiOpener(NukiDevice):
-    pass
+    def activate_rto(self, block=False):
+        return self._bridge.lock_action(
+            nuki_id=self.nuki_id,
+            action=const.ACTION_OPENER_ACTIVATE_RTO,
+            block=block,
+        )
+
+    def deactivate_rto(self, block=False):
+        return self._bridge.lock_action(
+            nuki_id=self.nuki_id,
+            action=const.ACTION_OPENER_DEACTIVATE_RTO,
+            block=block,
+        )
+
+    def electric_strike_actuation(self, block=False):
+        return self._bridge.lock_action(
+            nuki_id=self.nuki_id,
+            action=const.ACTION_OPENER_ELECTRIC_STRIKE_ACTUATION,
+            block=block,
+        )
+
+    def activate_continuous_mode(self, block=False):
+        return self._bridge.lock_action(
+            nuki_id=self.nuki_id,
+            action=const.ACTION_OPENER_ACTIVATE_CONTINUOUS,
+            block=block,
+        )
+
+    def deactivate_continuous_mode(self, block=False):
+        return self._bridge.lock_action(
+            nuki_id=self.nuki_id,
+            action=const.ACTION_OPENER_DEACTIVATE_CONTINUOUS,
+            block=block,
+        )
