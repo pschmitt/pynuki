91737bc53a1c5f1ab9c56440930752b1612ada24
Philipp Schmitt <philipp@schmitt.co>
2020-05-14 10:44:15 +0200

    Version 1.3.4

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import find_packages, setup
 
 
 setup(
-    name='pynuki',
-    version='1.3.3',
-    license='GPL3',
-    description='Python bindings for nuki.io bridges',
+    name="pynuki",
+    version="1.3.4",
+    license="GPL3",
+    description="Python bindings for nuki.io bridges",
     # long_description=open('README.rst').read(),
-    author='Philipp Schmitt',
-    author_email='philipp@schmitt.co',
-    url='https://github.com/pschmitt/pynuki',
+    author="Philipp Schmitt",
+    author_email="philipp@schmitt.co",
+    url="https://github.com/pschmitt/pynuki",
     packages=find_packages(),
-    install_requires=['requests'],
+    install_requires=["requests"],
 )
