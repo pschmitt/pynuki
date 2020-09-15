e69db4db6fae1b24bae9a24f4eab02014f523533
Pascal Reeb <github@schizopriest.de>
2020-09-15 14:14:14 +0200

    added doorsensor to NukiLock (#61)
    * added doorsensor to NukiLock
    
    * added unknown state to activated check
    
    * added None-Check (for V1 Locks) to activated check
    
    * check if door_sensor_state is set (v1 Locks)

--- pynuki/lock.py
+++ pynuki/lock.py
@@ -10,6 +10,25 @@ class NukiLock(NukiDevice):
     def is_locked(self):
         return self.state == const.STATE_LOCK_LOCKED
 
+    @property
+    def is_door_sensor_activated(self):
+        # Nuki v1 locks don't have a door sensor, therefore the
+        # door_sensor_state will is unset for them.
+        if (
+            not self.door_sensor_state
+            or self.door_sensor_state == const.STATE_DOORSENSOR_UNKNOWN
+        ):
+            return
+        return self.door_sensor_state != const.STATE_DOORSENSOR_DEACTIVATED
+
+    @property
+    def door_sensor_state(self):
+        return self._json.get("doorsensorState")
+
+    @property
+    def door_sensor_state_name(self):
+        return self._json.get("doorsensorStateName")
+
     def lock(self, block=False):
         return self._bridge.lock(nuki_id=self.nuki_id, block=block)
 
