16e159f2aa2da35522bddcf9dde44773d80f5acd
vauriga <77296558+vauriga@users.noreply.github.com>
2021-01-13 13:01:20 +0100

    Added additional device properties (#69)
    Added additional device properties

--- pynuki/lock.py
+++ pynuki/lock.py
@@ -28,6 +28,14 @@ class NukiLock(NukiDevice):
     def door_sensor_state_name(self):
         return self._json.get("doorsensorStateName")
 
+    @property
+    def battery_charge(self):
+        return self._json.get("batteryChargeState")
+
+    @property
+    def battery_critical_keypad(self):
+        return self._json.get("keypadBatteryCritical")
+
     def lock(self, block=False):
         return self._bridge.lock(nuki_id=self.nuki_id, block=block)
 
