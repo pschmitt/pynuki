5057962d8571b48e6eeb30d8a11e4280877f7778
Pascal Reeb <pascal@reeb.io>
2022-10-24 12:26:48 +0200

    feat(bridge-api): switch from hashed tokens to encrypted tokens

--- pynuki/utils.py
+++ pynuki/utils.py
@@ -5,15 +5,20 @@ import logging
 from datetime import datetime
 from random import randint
 
+import nacl.secret
+import nacl.utils
+
 logger = logging.getLogger("pynuki")
 
 
 def sha256sum(text):
-    return hashlib.sha256(text.encode("utf-8")).hexdigest()
+    return hashlib.sha256(text.encode("utf-8")).digest()
 
 
-def hash_token(token):
+def encrypt_token(digest):
+    nonce = nacl.utils.random(24)
     rnr = randint(0, 65535)
-    ts = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
-    hash_256 = sha256sum(f"{ts},{rnr},{token}")
-    return {"ts": ts, "rnr": rnr, "hash": hash_256}
+    ts = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ," + str(rnr))
+    box = nacl.secret.SecretBox(digest)
+    ctoken = box.encrypt(ts.encode("utf-8"), nonce)
+    return {"ctoken": str(ctoken.ciphertext.hex()), "nonce": str(nonce.hex())}
